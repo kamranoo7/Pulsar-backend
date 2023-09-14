90d5c05704954404a8488c3e6694d23609a3bfc2
MD Kamran khan <mohammadkkhan78@gmail.com>
2023-09-14 22:53:35 +0530

    s

--- Nexus/settings.py
+++ Nexus/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l0p1zwvjx4he+!1wp28zh=-fz#1hpkb=af04k&%d)hpzn=ra-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
